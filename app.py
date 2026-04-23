e0b826b147f00e39023e512b62e7208612a8bd22
Slyx22 <106899697+Slyx22@users.noreply.github.com>
2026-04-23 11:02:16 +0200

    Use environment variable for server port
    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def home():
     return "AI is running 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))  # 👈 THIS IS THE FIX
+    app.run(host="0.0.0.0", port=port)
